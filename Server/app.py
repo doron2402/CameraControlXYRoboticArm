f9ffa4aa7de89173e56471fd64e624e7af7918c1
doron segal <doron2402@gmail.com>
2016-12-31 16:49:47 -0800

    more server example and some UI work

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,6 @@
 # app.py or app/__init__.py
 from flask import Flask, render_template, jsonify
+import os 
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,13 +15,17 @@ def dashboard():
 
 @app.route('/about')
 def about():
+	# check device status we can ping the robot and check if its "alive"
 	return render_template('about.html')
 # Now we can access the configuration variables via app.config["VAR_NAME"].
 
 @app.route('/commands/<type>/<action>', methods=['POST'])
 def commands(type, action):
-    # name=request.form['yourname']
-    # email=request.form['youremail']
+    print("Command Type %s" % type)
+    print("Command Action %s" % action)
+    # example of running a linux command line
+    os.system("ls -lsf")
+    os.system("echo 'Doron is my king!!!'")
     res = { 'type': type, 'action': action }
     return jsonify(**res)
 
