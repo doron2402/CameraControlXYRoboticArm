2a672ee79f719be5d2e636348d360e32ed2ffa14
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-22 20:35:50 +0200

    ObjectTrackinByColor1.py

---
+++ PythonTemp/ObjectTrackinByColor1.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+
+
+
+def main():
+	# Take a frame
+	image = cv2.imread('./Server/static/images/image4_with_sample.jpg')
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    #cv2.imshow('frame',frame)
+    #cv2.imshow('mask',mask)
+    #cv2.imshow('res',res)
+    
+    cv2.imwrite('./Server/static/images/image4_result.jpg',res)
+    
+
+if (__name__ == "__main__"):
+	main()
