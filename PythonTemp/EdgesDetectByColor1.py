ea4d3a7df8e54d078c7e166c2db632b607cb27b2
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-22 23:32:03 +0200

    EdgeDetectByColor1.py syntax error

--- PythonTemp/EdgesDetectByColor1.py
+++ PythonTemp/EdgesDetectByColor1.py
@@ -23,21 +23,23 @@ def main():
     cv2.imwrite('image4_mask_result.jpg',res)
 
     #Hough Line Transform
+
     # detect circles in the image
+
     circles = cv2.HoughCircles(mask, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
     # ensure at least some circles were found
     if circles is not None:
-    # convert the (x, y) coordinates and radius of the circles to integers
-    circles = np.round(circles[0, :]).astype("int")
-    # loop over the (x, y) coordinates and radius of the circles
-    for (x, y, r) in circles:
+         # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
         # draw the circle in the output image, then draw a rectangle
         # corresponding to the center of the circle
         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
         cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-        
-    # show the output image
-    cv2.imwrite('image4_output.jpg',output)
+         
+        # show the output image
+        cv2.imwrite('image4_output.jpg',output)
 	
 
 
