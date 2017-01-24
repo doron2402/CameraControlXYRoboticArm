43889b99ecdedbd3a8e5077f0bfd85e5025ac5f6
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-24 21:13:42 +0200

    file CirclesCoordinate.py syntax

--- PythonTemp/CirclesCoordinate.py
+++ PythonTemp/CirclesCoordinate.py
@@ -24,28 +24,8 @@ def main():
         param1=50,param2=30,minRadius=0,maxRadius=0)
 
     
-    print ('x = ', circles[0] , 'y = ',circles[1] , 'Radius = ',circles[2])
+    print (circles)
     
-
-    '''#---------Detect Red Objects-------------
-    # define range of red color in HSV
-    lower_red = np.array([0,70,50])
-    upper_red = np.array([10,255,255])
-    # Threshold the HSV image to get only red colors
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(image,image, mask = mask)
-
-    lower_red = np.array([175,70,50])
-    upper_red = np.array([180,255,255])
-    
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res2 = cv2.bitwise_and(image,image, mask = mask)
-    
-    res3= cv2.bitwise_or(image,image, mask = mask)
-
-    cv2.imwrite('image4_red_result.jpg',res3)
-    */'''
 
 if (__name__ == "__main__"):
-	main()
+    main()
