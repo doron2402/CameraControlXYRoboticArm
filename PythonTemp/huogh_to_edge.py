4ad5949cc352737036523d95528b7dc3b4afd7d2
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-30 14:44:57 +0200

    huogh transform

--- PythonTemp/huogh_to_edge.py
+++ PythonTemp/huogh_to_edge.py
@@ -17,21 +17,22 @@ def main():
     lower_red = np.array([0,70,50])
     upper_red = np.array([10,255,255])
     # Threshold the HSV image to get only red colors
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
     # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(image,image, mask = mask)
+    #res = cv2.bitwise_and(image,image, mask = mask)
 
     lower_red = np.array([175,70,50])
     upper_red = np.array([180,255,255])
     
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res2 = cv2.bitwise_and(image,image, mask = mask)
+    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+    mask = cv2.bitwise_or(mask1, mask2, mask = None)
+    #res2 = cv2.bitwise_and(image,image, mask = mask)
     
-    res3= cv2.bitwise_or(image,image, mask = mask)
+    #res3= cv2.bitwise_or(image,image, mask = mask)
     #---------Detect edge-------------
     #edge = cv2.Canny(res3,100,200)
 
-    cv2.imwrite('two circles result.png',res3)
+    cv2.imwrite('two circles result.png',mask)
     img2 = cv2.imread('two circles result.png',0)
     #---------Detect circle-------------
     img2 = cv2.medianBlur(img2,5)
