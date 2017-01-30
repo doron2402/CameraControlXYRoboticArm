c0af3437a3ec882d7c16ca74a86a4a74bfe4c38c
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-30 12:15:42 +0200

    huogh transform

--- PythonTemp/ConvertRgbToGray.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-def main():
-	image = cv2.imread('pic_mountain.jpg')
-	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	cv2.imwrite('gray_image.png',gray_image)
-
-if (__name__ == "__main__"):
-	main()
--- PythonTemp/FindCircle.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-
-def main():
-    
-
-if (__name__ == "__main__"):
-    main()
