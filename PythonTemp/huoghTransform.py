55b286fe0738a2b0b6604666fd8705ef74805f07
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-30 11:06:46 +0200

    huogh transform

--- PythonTemp/huoghTransform.py
+++ PythonTemp/huoghTransform.py
@@ -1,24 +1,26 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image4_with_sample.jpg')
-img3 = img.copy()
+img = cv2.imread('image4_with_sample.jpg',0)
 
-img2 = cv2.medianBlur(img,5)
 
-gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+img = cv2.medianBlur(img,5)
 
-cimg = cv2.filter2D(gray, cv2.CV_8U, gb_kernel.transpose())
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-circles = cv2.HoughCircles(cimg,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)
+#cimg = cv2.filter2D(gray, cv2.CV_8U, gb_kernel.transpose())
+
+#circles = cv2.HoughCircles(img2,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)
+circles = cv2.HoughCircles(img2,cv2.HOUGH_GRADIENT,1,10, param1=100,param2=30,minRadius=5,maxRadius=50)
 
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
     # draw the outer circle
-	cv2.circle(img3,(i[0],i[1]),i[2],(0,255,0),2)
+	cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
     # draw the center of the circle
-	cv2.circle(img3,(i[0],i[1]),2,(0,0,255),3)
+	cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detected circles',img3)
+cv2.imshow('detected circles',cimg)
 cv2.waitKey(0)
+cv2.imwrite('detected circles.jpg',cimg)
 cv2.destroyAllWindows()
