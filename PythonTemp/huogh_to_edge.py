fd05a00998f14e8ad81ea2a380da1034eab2450b
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-30 14:51:16 +0200

    huogh transform

--- PythonTemp/huogh_to_edge.py
+++ PythonTemp/huogh_to_edge.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def main():
     # Take a frame
-    image = cv2.imread('two circles.png')
+    image = cv2.imread('red circles.png')
     image2 = image.copy()
     # Convert BGR to HSV
     hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
@@ -30,9 +30,9 @@ def main():
     
     #res3= cv2.bitwise_or(image,image, mask = mask)
     #---------Detect edge-------------
-    edge = cv2.Canny(mask,100,200)
+    #edge = cv2.Canny(mask,100,200)
 
-    cv2.imwrite('two circles result.png',edge)
+    cv2.imwrite('two circles result.png',mask)
     img2 = cv2.imread('two circles result.png',0)
     #---------Detect circle-------------
     img2 = cv2.medianBlur(img2,5)
