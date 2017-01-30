ae4b82fd0580935583d4b25af409d2634c7959e4
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-01-30 11:18:51 +0200

    huogh transform

--- PythonTemp/huoghTransform.py
+++ PythonTemp/huoghTransform.py
@@ -12,15 +12,20 @@ cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
 #circles = cv2.HoughCircles(img2,cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)
 circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,10, param1=100,param2=30,minRadius=5,maxRadius=50)
+if circles is None:
+	print "No circle found"
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-	cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-	cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+elif:
+	print circles
+	circles = np.uint16(np.around(circles))
+	for i in circles[0,:]:
+    	# draw the outer circle
+		cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    	# draw the center of the circle
+		cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detected circles',cimg)
-cv2.waitKey(0)
-cv2.imwrite('detected circles.jpg',cimg)
+	cv2.imshow('detected circles',cimg)
+	cv2.waitKey(0)
+	cv2.imwrite('detected circles.jpg',cimg)
+
 cv2.destroyAllWindows()
