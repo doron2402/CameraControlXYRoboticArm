877a5430b01c233fffafd6458ecc33ba342fc1d3
Mizrahi Amit <mizrahiamit28@gmail.com>
2017-02-06 20:39:20 +0200

    take Picture

---
+++ PythonTemp/take_picture.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+
+
+
+def main():
+    # Take a frame
+    os.system("fswebcam -r 640x480 --no-banner current_pic.jpg")
+    
+
+if (__name__ == "__main__"):
+	main()
